1e2b8935939d321978831233ad6529c4fdfa8e42
Lebrun Regis <lebrun.regis.formation@gmail.com>
2025-06-03 13:21:53 +0200

    Started implementing game over system

---
+++ Assets/_/Features/ScriptableObjects/Events/Runtime/Bool/SyncedBoolValue.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SyncedBoolValue : SyncedValue<bool>
+{
+    #region Publics
+        
+    #endregion
+        
+        
+    #region Unity Api
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+    #endregion
+        
+        
+    #region Main Methods
+        
+    #endregion
+        
+        
+    #region Utils
+        
+    #endregion
+        
+        
+    #region Private and Protected
+        
+    #endregion
+        
+        
+}
