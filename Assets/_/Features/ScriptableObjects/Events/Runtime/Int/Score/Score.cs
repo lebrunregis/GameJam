8db0572174ff377f079c9754d9b025619553f010
Lebrun Regis <lebrun.regis.formation@gmail.com>
2025-06-03 10:25:48 +0200

    Reset score on enable

--- Assets/_/Features/ScriptableObjects/Events/Runtime/Int/Score/Score.cs
+++ Assets/_/Features/ScriptableObjects/Events/Runtime/Int/Score/Score.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     public SyncedValue<int> currentScore;
+    public bool m_autoReset;
 
     public void IncreaseScore(int score)
     {
         currentScore.SourceValue += score;
+    }
+
+    public void ResetScore()
+    {
+        currentScore.SourceValue = 0;
+    }
+
+    private void OnEnable()
+    {
+        if (m_autoReset)
+        {
+            ResetScore();
+        }
     }
 }
