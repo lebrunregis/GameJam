4723fa8cce8483843026dd63af102db22d1a6863
Lebrun Regis <lebrun.regis.formation@gmail.com>
2025-06-03 15:00:09 +0200

    Added title screen

---
+++ Assets/_/Features/SceneLoader/SceneController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlayButtonPushed()
+    {
+        SceneManager.LoadScene("DevScene");
+    }
+    public void OnQuitButtonPushed()
+    {
+
+    }
+}
